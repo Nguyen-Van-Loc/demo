ea59965e377416d3db3d7a01b28b3b95fe84391d
Lộc Văn <loccosi3005@gmail.com>
2023-03-22 09:02:28 +0700

    add

--- sever.js
+++ sever.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const multer = require('multer');
 var fs = require('fs');
 
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // SET STORAGE
@@ -12,30 +13,33 @@ var storage = multer.diskStorage({
 
     destination: function (req, file, cb) {
         var dir = './upload';
-
+    
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true });
         }
         cb(null, 'upload')
+       
     },
 
     filename: function (req, file, cb) {
         let filename = file.originalname;
-        arr = filename.split('.');
-        let newFileName = '';
-        for (let i = 0; i < arr.length; i++) {
-            if (i != arr.length - 1) {
-                newFileName += arr[i];
-            } else {
-                newFileName += ('-' + Date.now()+'.'+arr[i]);
+            arr = filename.split('.');
+            let newFileName = '';
+            for (let i = 0; i < arr.length; i++) {
+                if (i != arr.length - 1) {
+                    newFileName += arr[i];
+                } else {
+                    newFileName += ('-' + Date.now()+'.'+"JPEG");
+                }
+    
             }
-
-        }
-        cb(null, newFileName)
+            cb(null, newFileName)
+       
     }
 })
 
-var upload = multer({ storage: storage })
+// var upload = multer({ storage: storage })
+var upload=multer({storage:storage,limits:{fileSize:1*1024*1024}})
 
 app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
     const file = req.file
@@ -46,6 +50,13 @@ app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
     }
     res.send(file)
 })
+app.use((err, req, res, next) => {
+    if (err instanceof multer.MulterError) { // Multer-specific errors
+        return res.send("Kich thuoc file lon hon 1mb")
+    } else { // Handling errors for any other cases from whole application
+        return res.send("Upload thành công")
+    }
+});
 
 //Uploading multiple files
 app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
@@ -61,6 +72,16 @@ app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/upload.html');
 });
+app.post('/upload/photo',upload.array('myImage'), (req,res ,next)=>{
+
+    const files = req.files
+    if (!files) {
+        const error = new Error('Please choose files')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.send(files)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
